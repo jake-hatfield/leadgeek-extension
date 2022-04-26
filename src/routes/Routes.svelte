339118f8b1790b84e8f63ba64c9a2d6a69b8a9f8
Jacob Hatfield <jacobmhatfield@gmail.com>
2022-04-26 13:11:29 -0500

    pre-routify

--- src/routes/Routes.svelte
+++ src/routes/Routes.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	// packages
-	import { Route, Router } from 'svelte-navigator';
+	import { Link, Route, Router } from 'svelte-navigator';
 
 	// components
 	import AuthLayout from '@components/layout/AuthLayout.svelte';
+	import Alert from '@components/utils/Alert.svelte';
 	import RouteGuard from '@routes/RouteGuard.svelte';
+	import Nav from '@components/layout/Nav.svelte';
 
 	// pages
 	import Dashboard from '@pages/index.svelte';
@@ -15,21 +17,22 @@
 </script>
 
 <Router basepath={isProd ? '/index.html' : '/'} primary={false}>
-	<AuthLayout>
-		<Route>
-			<h3>404</h3>
-		</Route>
-		<RouteGuard isPrivate={false} path="login">
-			<Login />
-		</RouteGuard>
-		<RouteGuard isPrivate={false} path="forgot-password">
-			<p>Forgot password</p>
-		</RouteGuard>
-		<RouteGuard path="">
-			<Dashboard />
-		</RouteGuard>
-		<RouteGuard path="issue-scanner">
-			<IssueScanner />
-		</RouteGuard>
-	</AuthLayout>
+	<Nav />
+	<main>
+		<div class="relative ">
+			<RouteGuard isPrivate={false} path="login">
+				<Login />
+			</RouteGuard>
+			<RouteGuard isPrivate={false} path="forgot-password">
+				<p>Forgot password</p>
+			</RouteGuard>
+			<RouteGuard isPrivate={true} path="">
+				<Dashboard />
+			</RouteGuard>
+			<RouteGuard isPrivate={true} path="issue-scanner">
+				<IssueScanner />
+			</RouteGuard>
+		</div>
+	</main>
+	<Alert />
 </Router>
