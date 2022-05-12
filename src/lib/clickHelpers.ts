823950a60ee19b2b68ef48af322cb716e3e1a421
Jacob Hatfield <jacobmhatfield@gmail.com>
2022-05-12 17:35:29 -0500

    scanner searching

--- src/lib/clickHelpers.ts
+++ src/lib/clickHelpers.ts
@@ -31,3 +31,29 @@ export const handleClickOutside = (
 		},
 	};
 };
+
+export const keyboardShortcut = (node: HTMLElement, params) => {
+	let handler;
+	const removeHandler = () => window.removeEventListener('keydown', handler),
+		setHandler = () => {
+			removeHandler();
+			if (!params) return;
+			handler = (e) => {
+				if (
+					!!params.alt != e.altKey ||
+					!!params.shift != e.shiftKey ||
+					!!params.control != (e.ctrlKey || e.metaKey) ||
+					params.code != e.code
+				)
+					return;
+				e.preventDefault();
+				params.callback ? params.callback() : node.click();
+			};
+			window.addEventListener('keydown', handler);
+		};
+	setHandler();
+	return {
+		update: setHandler,
+		destroy: removeHandler,
+	};
+};
