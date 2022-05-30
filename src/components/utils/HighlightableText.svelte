3ecf4ea214a1571b41b3fa2af9e2fee8aaf97cbb
Jacob Hatfield <jacobmhatfield@gmail.com>
2022-05-30 17:21:13 -0500

    we're doing it, reddit

---
+++ src/components/utils/HighlightableText.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	// packages
+	import highlightWords from 'highlight-words';
+
+	// props
+	export let text: string;
+	export let query: string;
+
+	$: chunks = highlightWords({ text, query });
+</script>
+
+<span aria-hidden="true">
+	{#each chunks as chunk (chunk.key)}
+		<span class:highlight={chunk.match}>{chunk.text}</span>
+	{/each}
+</span>
